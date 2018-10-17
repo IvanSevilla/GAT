75512685b33199c2c301b0a5bbd1c2f90cd55212
Iván Sevilla Ramos <ivi24.s.r@gmail.com>
2018-10-17 23:29:03 +0200

    Template in place

--- GeoApp/Geolib/geolib.h
+++ GeoApp/Geolib/geolib.h
@@ -8,6 +8,13 @@ class GEOLIBSHARED_EXPORT Geolib
 
 public:
     Geolib();
+    void LoadProject(string path);
+    void ReadFile(string path);
+    void ReadImage(string path);
+    void ReadMatrix(string path);
+    void ReadTif(string path);
+    glm::vec3 DotPCCalculus(glm::vec2 point);
+    glm::vec2 LensCorrection(glm::vec2 point);
 };
 
 #endif // GEOLIB_H
--- GeoApp/Geolib/geolib_global.h
+++ GeoApp/Geolib/geolib_global.h
@@ -2,7 +2,9 @@
 #define GEOLIB_GLOBAL_H
 
 #include <QtCore/qglobal.h>
-
+#include <string.h>
+#include "glm/glm.hpp"
+using namespace std;
 #if defined(GEOLIB_LIBRARY)
 #  define GEOLIBSHARED_EXPORT Q_DECL_EXPORT
 #else
